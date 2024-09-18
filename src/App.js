ff3cb0a7e846b3620f92211f62e8685722b73ac0
unknown <roshniyadavjgr@gmail.com>
2024-09-18 15:13:47 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import Home from './Home'
+import { Routes, Route} from 'react-router-dom'
+import SingleMovie from './SingleMovie'
+import Error from './Error'
+import "./App.css"
+
+const App=()=>{
+  return(
+    <div>
+      
+        <Routes>
+          <Route path ="/" element={<Home/>}/>
+          <Route path ="movie/:id" element={<SingleMovie/>}/>
+          <Route path ="*" element={<Error/>}/>
+        </Routes>
+      
+    </div>
+  )
+}
+export default App;
